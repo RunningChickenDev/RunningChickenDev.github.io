36b7be1a4b20cd15bb8be8c19477d0d184edfca2
MisterCavespider <cavespider12@gmail.com>
2017-04-04 19:48:55 +0200

    moved tracker to seperate file

--- MisterCavespider_js_workshop/trackerlib.js
+++ MisterCavespider_js_workshop/trackerlib.js
@@ -1,34 +1,42 @@
+/************************************************************/
 //includes jQuery
 var script = document.createElement('script');
 script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
-/////////////////////////////////////////////////////////////
+/************************************************************/
 
-//literal string, including https://
+/**
+ * literal string
+ */
 var trackerSite;
-
-function setTrackerSite(site) {
-  console.log("Setting new tracker site...");
-  trackerSite = site;
-}
+var dataHandler;
 
 function getTrackerSite() {
-  return trackerSite
+  	return trackerSite;
+}
+
+function setTrackerSite(site) {
+  	console.log("Setting new tracker site...");
+  	trackerSite = site;
+}
+
+function getDataHandler() {
+	return dataHandler;
+}
+
+function setDataHandler(handler) {
+	dataHandler = handler;
 }
 
 //returns json
 function request() {
-  console.log("Requesting from site: " + getTrackerSite());
-  
-  $.getJSON(trackerSite, function(rawdata) {
-    //might not be the fastest way
-    var data = JSON.parse(JSON.stringify(rawdata));
-    
-    writeData(data);
-  }
-}
+  	console.log("Requesting from site: " + getTrackerSite());
 
-function writeData(json) {
-  return 0;
+  	$.getJSON(trackerSite, function(rawdata) {
+    	//might not be the fastest way
+    	var data = JSON.parse(JSON.stringify(rawdata));
+
+    	dataHandler(data);
+  	}
 }
